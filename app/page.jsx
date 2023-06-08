458e8730c44f605518bb7cda5ea55654f59670e5
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2023-06-08 20:21:42 -0300

    Fetch data

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
-function IndexPage(){
+import Users from "@/components/Users"
+
+async function fetchUsers(){
+  const response = await fetch("https://reqres.in/api/users")
+  const data = await response.json()
+  
+  return data.data
+}
+
+async function IndexPage(){
+  const users = await fetchUsers()
   return (
-    <h1>Index Page</h1>
+    <>
+      <h1>Index Page</h1>
+      <Users users={users} />
+    </>
   )
 }
 
---
+++ components/Users.jsx
@@ -0,0 +1,21 @@
+function Users({users}){
+	return (
+		<>
+			<ul>
+        {
+          users.map((user) => (
+            <li key={user.id}>
+              <div>
+                <h5>{user.id} {user.first_name} {user.last_name}</h5>
+                <p>{user.email}</p>
+              </div>
+              <img src={user.avatar} alt="" />
+            </li>
+          ))
+        }
+      </ul>
+		</>
+	)
+}
+
+export default Users
