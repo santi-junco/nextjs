e979c4296d6cc2dbc873467203ad651f7c3fe36b
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2023-06-08 21:02:52 -0300

    User detail

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,9 +1,21 @@
-function User(){
-    return (
-        <>
-            <h1>User Detail</h1>
-        </>
-    )
+async function getUser(id){
+	const response = await fetch(`https://reqres.in/api/users/${id}`)
+	const data = await response.json()
+	
+	return data.data
+}
+
+async function User({params}){
+
+	const user = await getUser(params.id)
+	
+	return (
+		<>
+			<h1>{user.first_name} {user.last_name}</h1>
+			<p>{user.email}</p>
+			<img src={user.avatar} alt="" />
+		</>
+	)
 }
 
 export default User
